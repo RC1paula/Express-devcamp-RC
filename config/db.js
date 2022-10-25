ff1149e91dfc4a47675e9ce517ff6a6f1a5a5a3b
Your Name <you@example.com>
2022-10-25 09:24:49 -0500

    commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,27 @@
-const { connect } = require('../routes/BootcampRoutes')
 const sequelize = require('./seq')
+//Dependencia del Modelo
+const UserModel = require('../models/user')
 
+//Dependencia DataTypes
+const { DataTypes } = require('sequelize')
+
+const User = UserModel( sequelize , DataTypes )
 //Crear función asyncrona para generar conexión
 const conectarBD = async () =>{
     try {
         await sequelize.authenticate()
         console.log('Se estableción conexión')
+        //Seleccionar Users
+        const users = await User.findAll();
+        console.log(users)
+
+        //Crear 
+        const marco = await User.create({
+            name: "Marco",
+            email: "marcopolo@gmail",
+            password: "mar123",
+        });
+
     } catch (error) {
         console.log(error)
     }
